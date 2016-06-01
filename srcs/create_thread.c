8042cd259df4a4e94ba21be4c1f5d6561f4174ea
Clement DENIAU <cdeniau@e2r11p19.42.fr>
2016-06-01 15:58:23 +0200

    Implement thread creation for each philosopher

--- srcs/create_thread.c
+++ srcs/create_thread.c
@@ -6,7 +6,7 @@
 /*   By: cdeniau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/01 14:59:44 by cdeniau           #+#    #+#             */
-/*   Updated: 2016/06/01 15:33:17 by cdeniau          ###   ########.fr       */
+/*   Updated: 2016/06/01 15:57:15 by cdeniau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,14 @@ static void					*thread_start(void *arg)
 	char					*uargv;
 	char					*p;
 
-	*tinfo = arg;
+	tinfo = arg;
 	printf("Thread %d: top of stack near %p; argv_string=%s\n",
 			tinfo->thread_num, &p, tinfo->argv_string);
 	uargv = strdup(tinfo->argv_string);
 	if (uargv == NULL)
 		handle_error("strdup");
 	for (p = uargv; *p != '\0'; p++)
-		*p = toupper(*p);
+		*p = ft_toupper(*p);
 	return uargv;
 }
 
@@ -38,18 +38,17 @@ int							create_threads(void)
 
 	nb_threads = 7;
 	i = 0;
-	tinfo = malloc(sizeof ((struct thread_info) * nb_threads));
+	tinfo = malloc(sizeof (struct t_thread_info) * 7);
 	if (tinfo == NULL)
 		handle_error("malloc");
 	while (i++ < nb_threads)
 	{
-		tinfo[i].thread_num = i + 1;
-		tinfo[i].argv_string = ft_strdup("sample text"); // TODO : free
+		tinfo[i].thread_num = i;
+		tinfo[i].argv_string = ft_strdup("Sample Text"); // TODO : free
 		s = pthread_create(&tinfo[i].thread_id, NULL, &thread_start, &tinfo[i]);
 		if (s != 0)
 			handle_error_en(s, "pthread_create");
 	}
-	s = pthread_attr_destroy(&attr); // TODO : * 7
 	if (s != 0)
 		handle_error_en(s, "pthread_attr_destroy");
 	free(tinfo);
